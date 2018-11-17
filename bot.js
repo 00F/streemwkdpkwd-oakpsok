0a9763fcac95aaf16b6157af1a0c3bf147c78741
` 0ff <39999036+00F@users.noreply.github.com>
2018-11-17 20:12:53 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const developers = ['320423357709549568'];
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setGame(`من يحاول أن يخسرك ساعده.`,'https://www.twitch.tv/MeeRcY')
+  client.user.setStatus("idle")
+});
+
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!developers.includes(message.author.id)) return;
+
+if (message.content.startsWith('Mwt')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+  client.user.setStatus("idle")
+    message.channel.send(` ** ${argresult} \ ** `)
+} else 
+if (message.content.startsWith('Mls')) {
+client.user.setActivity(argresult , {type:'LISTENING'});
+  client.user.setStatus("idle")
+    message.channel.send(` ** ${argresult} \ ** `)
+} else 
+if (message.content.startsWith('Mst')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/MeeRcY");
+  client.user.setStatus("idle")
+   message.channel.send(` ** ${argresult} \ ** `)
+}
+});
+client.login(process.env.MLK_TKN);
